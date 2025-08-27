99faf66a042f2ecf869481ec757fde61036e8079
teetom3 <thomas.zeryouh@gmail.com>
2025-08-27 14:44:56 +0200

    test port 3000

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-// Importe le module Express
-const express = require('express');
-const app = express();
+import { app } from "./server.js";
 const port = 3001;
-
-// Première route : page d'accueil
-app.get('/', (req, res) => {
-  res.send('Bienvenue sur la page d\'accueil  saikou!');
-});
-
-// Deuxième route : une page "À propos"
-app.get('/about', (req, res) => {
-  res.send('Ceci est la page "À propos".');
-});
-
-// Troisième route : une route dynamique avec un paramètre
-app.get('/users/:name', (req, res) => {
-  const userName = req.params.name;
-  res.send(`Bonjour, ${userName} !`);
-});
-
-// Démarre le serveur et le fait écouter sur le port spécifié
 app.listen(port, () => {
-  console.log(`Le serveur écoute sur http://localhost:${port}`);
+  console.log(`Running in http://localhost:${port}`);
 });
