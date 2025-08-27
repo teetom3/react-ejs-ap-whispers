90ab1c9c9a65135a6f0e5bf8aab8d014afc2d25b
teetom3 <thomas.zeryouh@gmail.com>
2025-08-27 10:14:04 +0200

    add type module package json and add test whispers

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { getAll, getById, create, updateById, deleteById } from './store.js';
 const app = express();
+app.use(express.static('public'))
 app.use(bodyParser.json());
-app.get("/api/v1/whisper", (req, res) => {
-  res.json([]);
+
+app.set('view engine' , 'ejs')
+
+app.get('/about' , async (req, res) => {
+    const whispers = await getAll()
+    res.render('about' , { whispers })
+})
+app.get("/api/v1/whisper", async  (req, res) => {
+  const whispers = await getAll()
+  res.json(whispers);
 });
 app.get("/api/v1/whisper/:id", (req, res) => {
   const id = parseInt(req.params.id);
